bda15098ff4980d72e04282af9f5281b47d1d033
Grace Finnick <35904299+gracefinnick@users.noreply.github.com>
2018-07-20 15:14:07 -0400

    iOS specific files

---
+++ PhoneCallApp/PhoneCallApp.iOS/PhoneDialer.iOS.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Threading.Tasks;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using PhoneCallApp.iOS;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+
+namespace PhoneCallApp.iOS
+{
+    class PhoneDialer : IDialer
+    {
+        public Task<bool> DialAsync(string number)
+        {
+            return Task.FromResult(
+                UIApplication.SharedApplication.OpenUrl(
+                new NSUrl("tel:" + number))
+            );
+        }
+    }
+}
